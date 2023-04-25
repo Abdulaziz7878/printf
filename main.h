e37429e000eaa7110103154ec8563c6b6f53ea55
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-04-25 11:29:40 -0700

    main

--- main.h
+++ main.h
@@ -1,24 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
+
 #include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 #define DATA_TYPE
-#define UNUSED(x) (void)(x)
-#define B_SIZE 1024
-/**
- * struct fmt - create struct
- * @fmt: type specifier char
- * @fn: function
- */
-typedef struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[]);
-} fmt_t;
 
 int _length_mods(const char *s, int j);
 
@@ -40,21 +28,6 @@ struct conversion
 	int (*f)(va_list, char, int);
 };
 typedef struct conversion conv_list;
-int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list list, char buffer[]);
-/****formats****/
-int print_char(va_list list, char buffer[]);
-int print_string(va_list list, char buffer[]);
-int print_percent(va_list list, char buffer[]);
-int print_int(va_list list, char buffer[]);
-int print_binary(va_list list, char buffer[]);
-/****formats2****/
-int print_pointer(va_list list, char buffer[]);
-/****custom****/
-int print_reverse(va_list list, char buffer[]);
-int print_rot13(va_list list, char buffer[]);
-int print_unprintable(va_list list, char buffer[]);
-int add_hex(char c, char buffer[], int i);
 int _printf(const char *format, ...);
 int call_funcs(conv_list *conversion,
 		va_list conv, const char *format);
