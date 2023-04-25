413a4c6b9e03cd571e5d31dfb2519ba32c1aa233
Abdulaziz7878 <abdulazizdegefa78@gmail.com>
2023-04-25 11:43:08 -0700

    my first commit

--- main.h
+++ main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdio.h>
 
 #define UNUSED(x) (void)(x)
 #define B_SIZE 1024
